c93753d4f1d92153ef98ffbd227fbc01df93ab68
nicolas.roch <nicolas.roch@neel.cnrs.fr>
2019-11-07 15:26:15 +0100

    first attempt to write Redpitaya drivers for Qcodes

---
+++ redpitaya_qcodes.py
@@ -0,0 +1,10 @@
+# This is the RedPitaya driver for QCodes
+
+def hello():
+    """ Print "Hello world" and return nothing """
+    print("Hello world!")
+    
+    
+# Main prgram starts here
+ 
+hello()
