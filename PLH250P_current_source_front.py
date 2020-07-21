5b262964ff43b3be8034d925f690192c1840aeaa
nicolas.roch <nicolas.roch@neel.cnrs.fr>
2020-07-21 19:48:19 +0200

    added power on redundancy check and status check

--- PLH250P_current_source_front.py
+++ PLH250P_current_source_front.py
@@ -9,6 +9,8 @@ from time import sleep
 
 
 def current_set(current_source, I):
+	if current_source.status() == 'off':
+		power_up(current_source)
 	I_0=current_source.current()
 	steps=int(abs(I_0-I))*10
 	for i in range(0,steps):
@@ -20,7 +22,7 @@ def current_set(current_source, I):
 
 def power_up(current_source):
 	current_source.current_range('low')
-	if current_source.status() == 'off'
+	if current_source.status() == 'off':
 		current_source.status('on')
 		current_set(current_source, 0)
 		setstate('pos')
