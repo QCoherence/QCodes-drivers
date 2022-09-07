ec3611aec93027b3f2f1295515ff8eb4a9660285
sionludibloch <sionludi.bloch@gmail.com>
2022-09-07 15:37:52 +0200

    Removed rfsoc drivers and helloworld

--- hello_world.py
+++
@@ -1,9 +0,0 @@
-# Demo file for Spyder Tutorial
-# Hans Fangohr, University of Southampton, UK
-
-def hello():
-    """Print "Hello World" and return None."""
-    print("Hello World!")
-
-# Main program starts here
-hello()
--- rfSoC_gui.py
+++
@@ -1,4 +0,0 @@
-import serial
-
-rfSoC = serial.Serial(port='COM7', baudrate=115200)
-
