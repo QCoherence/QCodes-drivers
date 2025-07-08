3f74afae676673da64b7b74cfde877ed968dee69
sionludibloch <sionludi.bloch@gmail.com>
2025-07-08 17:38:12 +0200

    Fix not fully implemented methods

--- Agilent_E8257D.py
+++ Agilent_E8257D.py
@@ -1,12 +1,9 @@
-# Last updated on 14 Jun 2021
-#                     -- Dorian
+# Last updated on 07 Jul 2025
+#                     -- J-S
 
 
-import qcodes as qc
-from qcodes import (Instrument, VisaInstrument,
-ManualParameter, MultiParameter, Parameter,
-validators as vals)
-from qcodes.instrument.channel import InstrumentChannel
+from qcodes import (VisaInstrument, Parameter, validators as vals)
+from qcodes.utils.helpers import create_on_off_val_mapping
 import logging
 
 from numpy import pi
@@ -22,12 +19,7 @@ class FreqSweep(Parameter):
         super().__init__(*args, **kwargs)
 
     def get_raw(self):
-
-        start=self._instrument.freq_start.get()
-        stop=self._instrument.freq_stop.get()
-        step=self._instrument.freq_step.get()
-
-        return np.arange(start,stop+step,step)
+        return np.linspace(self._instrument.freq_start(), self._instrument.freq_stop(), self._instrument.freq_points())
 
 
 class E8257D(VisaInstrument):
@@ -51,17 +43,16 @@ class E8257D(VisaInstrument):
                             )
 
         self.add_parameter( name = 'power',
-                            label = 'Output power in dBm',
+                            label = 'Output power',
                             vals = vals.Numbers(-135,25),
                             unit   = 'dBm',
                             set_cmd='POW:AMPL ' + '{:.12f}',
                             get_cmd='POW:AMPL?',
                             get_parser=float,
-                            set_parser =self.warn_over_range
                             )
 
         self.add_parameter( name = 'phase',
-                            label = 'Output phase in Rad',
+                            label = 'Output phase',
                             vals = vals.Numbers(-2*pi,2*pi),
                             unit   = 'Rad',
                             set_cmd= 'PHASE ' + '{:.12f}',
@@ -72,12 +63,10 @@ class E8257D(VisaInstrument):
 
         self.add_parameter( name = 'status',
                             label = 'Output on/off',
-                            vals = vals.Enum('ON','OFF'),
-                            unit   = 'NA',
+                            vals = vals.Bool(),
+                            val_mapping=create_on_off_val_mapping(on_val=1, off_val=0),
                             set_cmd='OUTP ' + '{}',
                             get_cmd='OUTP?',
-                            set_parser =self.easy_read_status,
-                            get_parser=self.easy_read_status_read
                             )
 
         self.add_parameter( name = 'freq_start',
@@ -98,27 +87,18 @@ class E8257D(VisaInstrument):
                             get_parser=float
                             )
 
-        self.add_parameter( name = 'freq_step',
-                            label = 'Sweep: frequency step',
-                            vals = vals.Numbers(0.,20e9),
-                            unit   = 'Hz',
-                            set_cmd= 'FREQ:STEP ' + '{:.12f}' + 'Hz',
-                            get_cmd='FREQ:STEP?',
-                            get_parser=float
-                            )
-
         self.add_parameter( name = 'freq_points',
                             label = 'Sweep: frequency points',
-                            vals = vals.Numbers(2,20e9),
+                            vals = vals.Numbers(2, 20*10**9),
                             unit   = 'Hz',
                             set_cmd='SWEep:POINts ' + '{:.12f}',
                             get_cmd='SWE:POIN?',
-                            get_parser=float
+                            get_parser=int
                             )
 
         self.add_parameter( name = 'dwell_time',
                             label = 'Sweep: dwell time',
-                            vals = vals.Numbers(5e-3,1000),
+                            vals = vals.Numbers(1e-3,1000),
                             unit   = 's',
                             set_cmd='SWE:DWEL ' + '{:.12f}' + 's',
                             get_cmd='SWE:DWEL?',
@@ -126,20 +106,19 @@ class E8257D(VisaInstrument):
                             )
 
         self.add_parameter( name = 'sourcemode',
-        					label = 'Set source mode',
-        					vals = vals.Enum('CW','sweep'),
-        					set_cmd='SOURce:FREQuency:MODE '+ '{}',
-        					get_cmd='SOURce:FREQuency:MODE?',
-        					set_parser =self.set_freqsweep
-        					)
+                            label = 'Set source mode',
+                            vals = vals.Enum('CW','sweep'),
+                            set_cmd='SOURce:FREQuency:MODE '+ '{}',
+                            get_cmd='SOURce:FREQuency:MODE?',
+                            set_parser =self.set_freqsweep
+                            )
 
         self.add_parameter( name = 'sweepmode',
-        					label = 'Set frequency sweep mode',
-        					vals = vals.Enum('AUTO','SINGLE'),
-        					set_cmd='TRIG:FSW:SOUR '+ '{}',
-        					get_cmd='TRIG:FSW:SOUR?',
-        					set_parser =self.set_sweepmode
-        					)
+                            label = 'Set frequency sweep mode',
+                            vals = vals.Enum('AUTO','SINGLE'),
+                            set_cmd=self.set_sweepmode,
+                            get_cmd=self.get_sweepmode,
+                            )
         
         self.add_parameter( name = 'spacing_freq',
                             label='Set spacing mode of frequency sweep',
@@ -167,26 +146,6 @@ class E8257D(VisaInstrument):
     def deg_to_rad(self, theta):
 
         return float(theta)*pi/180.0
-
-    def easy_read_status(self, status):
-
-        if(status=='ON'):
-            ret=1
-        elif(status=='OFF'):
-            ret=0
-        return ret
-
-    def easy_read_status_read(self, status):
-        if(status=='1'):
-            ret='ON'
-        elif(status=='0'):
-            ret='OFF'
-        return ret
-
-    def warn_over_range(self, power):
-        if power>25:
-            log.warning('Power over range (limit to 16 dBm).')
-        return power
 
     def set_freqsweep(self, freqsweep='off'):
         '''
@@ -226,6 +185,14 @@ class E8257D(VisaInstrument):
             raise ValueError('Output status not specified : %s' % stat)
         return
 
+    def get_sweepmode(self):
+        '''
+        Get if we are in sweep mode continuous or single
+        '''
+        if self.ask('INITiate:CONTinuous?') == "0":
+            return "SINGLE"
+        else:
+            return "AUTO"
 
     def set_sweepmode(self, sweepmode='single'):
         '''
@@ -243,12 +210,11 @@ class E8257D(VisaInstrument):
             self.write('TRIGger:SOURce IMMediate')
             self.write('INITiate:CONTinuous ON')
 
-        # difference not implemented yet
         elif sweepmode.upper() in ('SINGLE'):
             self.write('SWE:MODE AUTO')
             self.write('SWEep:GENeration STEPped')
             self.write('TRIGger:SOURce IMMediate')
-            self.write('INITiate:CONTinuous ON')
+            self.write('INITiate:CONTinuous OFF')
         else:
             raise ValueError('set_sweepmode(): can only set AUTO or SINGLE')
 
@@ -262,20 +228,7 @@ class E8257D(VisaInstrument):
             None
         '''
 
-        self.write('FREQ:MODE SWE')
-
-
-    def restartsweep(self):
-        '''
-        Restart the frequency sweep.
-
-        Input:
-            None
-        Output:
-            None
-        '''
-
-        self.write('SOUR:SWE:RES')
+        self.write(':TSWeep')
 
     def set_gui_update(self, update='ON'):
         '''
@@ -289,30 +242,7 @@ class E8257D(VisaInstrument):
             None
         '''
 
-        self.write('DISP:REM %s' % update)
-
-
-    def query(self, cmd):
-        res= self.query(cmd + '?')
-        print(res)
-        return(res)
-
-    def tell(self, cmd):
-        self.write(cmd)
-
-    def sweepmode_set(self,sweepmode):
-        if sweepmode == 'auto':
-            ret = 'AUTO'
-        else:
-            ret = 'SING'
-        return ret
-
-    def sweepmode_get(self,sweepmode):
-        if sweepmode == 'AUTO':
-            ret = 'auto'
-        else:
-            ret = 'single'
-        return ret
+        self.write(f'DISP:REM {update}')
 
     def reset(self):
         '''
